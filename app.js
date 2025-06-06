d2e8f83002350ecff3991eafb3389820b3164fec
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-06-06 12:45:21 -0400

    change session duration

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require("morgan");
 const Sequelize = require("./db.connection");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var loginRouter = require("./routes/login");
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -1,82 +1,84 @@
 /* BEGIN db initialization */
 const { Op } = require("sequelize");
-const { v4: uuidv4 } = require('uuid');
-const moment = require('moment');
+const { v4: uuidv4 } = require("uuid");
+const moment = require("moment");
 const Sequelize = require("../db.connection");
-const Session = require("../models/session.model")(Sequelize.connection, Sequelize.library);
+const Session = require("../models/session.model")(
+  Sequelize.connection,
+  Sequelize.library
+);
 
 /* END db initialization */
 
 // Create session for user
 exports.create = async (id) => {
+  let validity = moment().add(60, "minutes").format("YYYY-MM-DD HH:mm:ss");
+  const obj = {
+    token: uuidv4(),
+    validUntil: validity,
+    userId: id,
+  };
+  console.log(validity);
 
-    let validity = moment().add(25,"minutes").format("YYYY-MM-DD HH:mm:ss")
-    const obj = {
-        token: uuidv4(),
-        validUntil: validity,
-        userId: id
-    };
-    console.log(validity)
-
-    // Save in the database
-    var result = {};
-    await Session.create(obj)
-        .then(data => {
-            result = data
-        })
-        .catch(e => {
-            console.log("error", e)
-        });
-    return result;
+  // Save in the database
+  var result = {};
+  await Session.create(obj)
+    .then((data) => {
+      result = data;
+    })
+    .catch((e) => {
+      console.log("error", e);
+    });
+  return result;
 };
 
 // Get session by user id
 exports.findByUserId = async (id) => {
-    var condition = id ? { userId: { [Op.eq]: id } } : null;
-    var result = {};
-    await Session.findOne({ where: condition })
-    .then(data => {
-        result = data
+  var condition = id ? { userId: { [Op.eq]: id } } : null;
+  var result = {};
+  await Session.findOne({ where: condition })
+    .then((data) => {
+      result = data;
     })
-    .catch(e => {
-        console.log("Error", e)
-    })
-    return result
+    .catch((e) => {
+      console.log("Error", e);
+    });
+  return result;
 };
 
 // Get session by token
 exports.findByToken = async (token) => {
-    var condition = token ? { token: { [Op.eq]: token } } : null;
-    var result = {};
-    await Session.findOne({ where: condition })
-    .then(data => {
-        result = data
+  var condition = token ? { token: { [Op.eq]: token } } : null;
+  var result = {};
+  await Session.findOne({ where: condition })
+    .then((data) => {
+      result = data;
     })
-    .catch(e => {
-        console.log("Error", e)
-    })
-    return result
+    .catch((e) => {
+      console.log("Error", e);
+    });
+  return result;
 };
 
 // Delete a record with a certain id
 exports.delete = (id) => {
-    Session.destroy({
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Record was deleted successfully!"
-                });
-            } else {
-                res.send({
-                    message: `Cannot delete record with id=${id}. Maybe record was not found!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Could not delete record with id=" + id
-            });
+  Session.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Record was deleted successfully!",
         });
+      } else {
+        res.send({
+          message: `Cannot delete record with id=${id}. Maybe record was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete record with id=" + id,
+      });
+    });
 };
